8655307caa92f2753e98e7880ebe405d1375034f
Bence <pirosgyb@gmail.com>
2024-06-28 13:54:00 +0200

    Rename application level theme

--- app/src/main/java/com/calmflame/codelab_jetpack_compose_basics/ui/ui/theme/Theme.kt
+++ app/src/main/java/com/calmflame/codelab_jetpack_compose_basics/ui/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.calmflame.codelab_jetpack_compose_basics.ui.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun CodelabJetPackComposeBasicsTheme(
+fun BasicsCodelabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
