f42687ac98e39e91d6a44200dc00b9b5da0c74b2
Bence <pirosgyb@gmail.com>
2024-06-29 09:32:47 +0200

    Change button text on button click

--- app/src/main/java/com/calmflame/codelab_jetpack_compose_basics/ui/MainActivity.kt
+++ app/src/main/java/com/calmflame/codelab_jetpack_compose_basics/ui/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,6 +46,7 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expended = remember { mutableStateOf(false) }
     Surface(
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp),
         color = MaterialTheme.colorScheme.primary
@@ -57,8 +60,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(text = "Hello ")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            ElevatedButton(onClick = { expended.value = !expended.value }) {
+                val text = if (expended.value) "Show less" else "Show more"
+                Text(text = text)
             }
         }
     }
